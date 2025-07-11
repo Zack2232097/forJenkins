c77c6ad9ddc5999e14922c3fe3823b3517dadfaa
Zack2232097 <152576598+Zack2232097@users.noreply.github.com>
2025-07-11 08:25:16 +0800

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -2,5 +2,6 @@ import requests
 
 def test_add():
     res = requests.get("http://127.0.0.1:5001/add?a=1&b=2")
+    print(res.json())
     assert res.status_code == 200
     assert res.json()['result'] == 3
